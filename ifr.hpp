94ea40b6232ec75234abc0803bc9fc47a951816e
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-05-15 22:52:14 +0200

    Added std::system_error exceptions

--- ifr.hpp
+++ ifr.hpp
@@ -39,6 +39,7 @@
 #include <stdexcept>
 #include <cerrno>
 #include <memory>
+#include <system_error>
 
 #include <string-utils.hpp>  // more
 #include <iomanip.hpp>       // more
@@ -67,7 +68,7 @@ namespace net {
         flags() const 
         {
             if (ioctl(ifr::sock_(), SIOCGIFFLAGS, &m_ifreq_io) < 0)
-                throw std::runtime_error("SIOCGIFFLAGS");
+                throw std::system_error(errno, std::generic_category());
 
             return m_ifreq_io.ifr_flags;
         }
@@ -104,10 +105,10 @@ namespace net {
             strncpy(m_ifreq_io.ifr_data, (char *) &req, sizeof(req));
 
             if (ioctl(sock_(), SIOCETHTOOL, &m_ifreq_io) == -1) {
-                throw std::runtime_error("SIOCETHTOOL");
+                throw std::system_error(errno, std::generic_category());
             } 
            
-            return drvinfo; 
+            return std::move(drvinfo); 
         }
 
         std::unique_ptr<ethtool_cmd> 
@@ -119,10 +120,10 @@ namespace net {
             m_ifreq_io.ifr_data = reinterpret_cast<__caddr_t>(ecmd.get());
 
             if (ioctl(sock_(), SIOCETHTOOL, &m_ifreq_io) == -1) {
-                throw std::runtime_error("SIOCETHTOOL");
+                throw std::system_error(errno, std::generic_category());
             } 
         
-            return ecmd;   
+            return std::move(ecmd);   
         }
 
         bool
@@ -133,7 +134,7 @@ namespace net {
 
             m_ifreq_io.ifr_data = reinterpret_cast<__caddr_t>(&edata);
             if (ioctl(sock_(), SIOCETHTOOL, &m_ifreq_io) == -1) {
-                throw std::runtime_error("SIOCETHTOOL");
+                throw std::system_error(errno, std::generic_category());
             }
             
             return edata.data;    
@@ -143,7 +144,7 @@ namespace net {
         mac() const 
         {
             if (ioctl(sock_(), SIOCGIFHWADDR, &m_ifreq_io) == -1) {
-                throw std::runtime_error("ioctl: SIOCGIFHWADDR");
+                throw std::system_error(errno, std::generic_category());
             }
             struct ether_addr *eth_addr = (struct ether_addr *) & m_ifreq_io.ifr_addr.sa_data;
             return  ether_ntoa(eth_addr);
@@ -153,7 +154,7 @@ namespace net {
         mtu() const
         {
             if (ioctl(sock_(), SIOCGIFMTU, &m_ifreq_io) == -1 ) {
-                throw std::runtime_error("ioctl: SIOCGIFMTU");
+                throw std::system_error(errno, std::generic_category());
             }
             return m_ifreq_io.ifr_mtu;
         }
@@ -163,7 +164,7 @@ namespace net {
         metric() const
         {
             if (ioctl(sock_(), SIOCGIFMETRIC, &m_ifreq_io) == -1 ) {
-                throw std::runtime_error("ioctl: SIOCGIFMETRIC");
+                throw std::system_error(errno, std::generic_category());
             }
             return m_ifreq_io.ifr_metric ? m_ifreq_io.ifr_metric : 1;
         }
@@ -193,7 +194,7 @@ namespace net {
 
             FILE *f;
             if ( (f=fopen(proc::IFINET6,"r")) == NULL)   
-                throw std::runtime_error("PROC_IFNET6");
+                throw std::system_error(errno, std::generic_category());
             
             while (fscanf(f, "%4s%4s%4s%4s%4s%4s%4s%4s %02x %02x %02x %02x %20s\n",
                           addr6p[0], addr6p[1], addr6p[2], addr6p[3],
@@ -246,7 +247,7 @@ namespace net {
         map() const
         {
             if (ioctl(sock_(), SIOCGIFMAP, &m_ifreq_io) == -1 ) {
-                throw std::runtime_error("ioctl: SIOCGIFMAP");
+                throw std::system_error(errno, std::generic_category());
             }
             return m_ifreq_io.ifr_ifru.ifru_map; 
         }
@@ -255,7 +256,7 @@ namespace net {
         txqueuelen() const
         {
             if (ioctl(sock_(), SIOCGIFTXQLEN, &m_ifreq_io) == -1 ) {
-                throw std::runtime_error("ioctl: SIOCSIFTXQLEN");
+                throw std::system_error(errno, std::generic_category());
             }
             return m_ifreq_io.ifr_qlen; 
         }
@@ -343,7 +344,7 @@ namespace net {
         {
             static int sock = socket(AF_INET, SOCK_DGRAM, 0);
             if (sock == -1)
-                throw std::runtime_error("socket");
+                throw std::system_error(errno, std::generic_category());
             return sock;
         }
 
